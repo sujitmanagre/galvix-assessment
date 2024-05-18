5b85a7ead9602962e6e1ebdfaa2af43ac4936526
Sujit Managre <sujitmanagre2000@gmail.com>
2024-05-18 06:33:31 +0530

    first commit

---
+++ src/main/java/com/galvix/galvixassessment/GalvixAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.galvix.galvixassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalvixAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalvixAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvix/galvixassessment/GalvixAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvix.galvixassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalvixAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
